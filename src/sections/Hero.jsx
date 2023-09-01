c59e336ba513edd52b23e77128577e888f034e54
mohammad jalifathi <m.h.jalifathi@gmail.com>
2023-09-01 16:41:29 +0330

    popularSectioon

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -3,14 +3,16 @@ import { arrowRight } from '../assets/icons'
 import { statistics, shoes } from './../constants/index';
 import { bigShoe1 } from '../assets/images'
 import ShoeCard from "../components/ShoeCard";
-
+import { useState } from "react";
 
 const Hero = () => {
+    const [bigShoeImg, setBigShoeImg] = useState(bigShoe1);
+
     return (
         <section id="home"
             className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container">
 
-            <div className='relative xl:w-2/5 flex flex-col justify-center items-start w-full  max-xl:padding-x pt-28'>
+            <div className='relative xl:w-2/5 flex flex-col justify-center items-start w-full     max-xl:padding-x pt-28'>
                 <p className='text-xl font-montserrat text-coral-red'>
                     Our Summer collections
                 </p>
@@ -39,17 +41,26 @@ const Hero = () => {
                         </div>
                     ))}
                 </div>
-            </div> 
-            <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center ">
-                <img src={bigShoe1} alt="big shoe" width={610} height={500} className="object-contain relative z-10" />
+            </div>
 
-                <div>
+
+            <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center ">
+
+                <img
+                    src={bigShoeImg}
+                    alt="big shoe"
+                    width={610}
+                    height={500}
+                    className="object-contain relative z-10"
+                />
+
+                <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6">
                     {shoes.map((shoe) => (
                         <div key={shoe}>
                             <ShoeCard
                                 imgUrl={shoe}
-                                changeBigShoeImg={() => { }}
-                                bigShoeImg='' />
+                                changeBigShoeImg={(shoe) => { setBigShoeImg(shoe) }}
+                                bigShoeImg={bigShoeImg} />
                         </div>
                     ))}
                 </div>
